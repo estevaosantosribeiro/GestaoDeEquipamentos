fe41e088e55d54360633a54aa44ac1037827f9c6
estevao-santos-ribeiro <estevaosantos@uniplaclages.edu.br>
2025-04-07 00:00:10 -0300

    Implementado edição e exclusão de equipamentos

---
+++ GestaoDeEquipamentos.ConsoleApp/GeradorIds.cs
@@ -0,0 +1,13 @@
+﻿namespace GestaoDeEquipamentos.ConsoleApp;
+
+public static class GeradorIds
+{
+    public static int IdEquipamentos = 0;
+
+    public static int GerarIdEquipamento()
+    {
+        IdEquipamentos++;
+
+        return IdEquipamentos;
+    }
+}
